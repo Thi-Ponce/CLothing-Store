61b6900d040cb17a12bcae22cbf332f43dffd009
Thi-Ponce <thi.pponce@gmail.com>
2022-08-16 18:24:52 -0300

    add styling and style categories

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
+import './categories.styles.scss';
+
 const App = () => {
   const categories = [
     {
       title: 'Hats',
       id: 1,
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       title: 'Jackets',
       id: 2,
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       title: 'Sneakers',
       id: 3,
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       title: 'Womens',
       id: 4,
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       title: 'Mens',
       id: 5,
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
-          <div className="bg-image" />
-          <div className="category-body">
+      {categories.map(({ title, id, imageUrl }) => (
+        <div key={id} className="category-container">
+          <div
+            className="background-image"
+            style={{ backgroundImage: `url(${imageUrl})` }}
+          />
+          <div className="category-body-container">
             <h2>{title}</h2>
             <p>Search</p>
           </div>
