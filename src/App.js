f45837e35aab39ec79b4eea14e774176fb47c8ca
Thi-Ponce <thi.pponce@gmail.com>
2022-08-16 18:34:50 -0300

    create and implement category component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './categories.styles.scss';
+import CategoryItem from './components/category/category-item.component';
 
 const App = () => {
   const categories = [
@@ -31,17 +32,8 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div
-            className="background-image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Search</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
