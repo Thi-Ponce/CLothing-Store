90541aef7262bb16a7451ae975f521401601b624
Thi-Ponce <thi.pponce@gmail.com>
2022-08-17 19:33:38 -0300

    change fonts, delete package-lock, fix app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import Category from './components/categories/categories.component';
-import CategoryItem from './components/category/category-item.component';
 
-const App = () => (
-  <Category>
-    <CategoryItem key="id" />
-  </Category>
-);
+const App = () => <Category />;
 
 export default App;
