203ca5f2a37d5dd276eac899d4d84351cca4d83a
Thi-Ponce <thi.pponce@gmail.com>
2022-08-18 17:32:41 -0300

    add styling to navbar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,17 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
 
 const Navigation = () => (
   <>
     <div className="navigation">
       <Link to="/" className="logo-container">
-        <div>Logo</div>
+        <Logo className="logo" />
       </Link>
       <div className="nav-links-container">
         <Link to="/shop" className="nav-link">
-          Shop
+          SHOP
         </Link>
       </div>
     </div>
